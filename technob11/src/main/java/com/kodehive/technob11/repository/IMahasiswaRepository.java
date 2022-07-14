7d20610543bb51200f35e8a896dfb93796573756
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2022-07-14 10:58:25 +0700

    Materi Repo Day 9 - 2

---
+++ technob11/src/main/java/com/kodehive/technob11/repository/IMahasiswaRepository.java
@@ -0,0 +1,8 @@
+package com.kodehive.technob11.repository;
+
+import com.kodehive.technob11.model.MahasiswaModel;
+
+public interface IMahasiswaRepository {
+	public int insert(MahasiswaModel model);
+	
+}
---
+++ technob11/src/main/java/com/kodehive/technob11/repository/impl/MahasiswaRepository.java
@@ -0,0 +1,21 @@
+package com.kodehive.technob11.repository.impl;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kodehive.technob11.model.MahasiswaModel;
+import com.kodehive.technob11.repository.IMahasiswaRepository;
+
+@Repository
+public class MahasiswaRepository implements IMahasiswaRepository {
+
+	JdbcTemplate jdbc;
+
+	@Override
+	public int insert(MahasiswaModel model) {
+		var query = "insert into t_mahasiswa(name, gender, alamat, spp)" + "values (?,?,?,?)";
+		return jdbc.update(query,
+				new Object[] { model.getName(), model.getGender(), model.getAlamat(), model.getSpp() });
+	}
+
+}
